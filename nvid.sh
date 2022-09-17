66a55a9fe7a94e59dbae00e8cd4c3facc6ddfa16
xeroxatpro <xeroxatpro@gmail.com>
2022-09-17 04:05:44 +0300

    Ubuntu İgnore Script in Shell

--- nvid.sh
+++ nvid.sh
@@ -17,7 +17,7 @@ wget https://developer.download.nvidia.com/compute/cuda/11.7.1/local_installers/
 sudo dpkg -i cuda-repo-ubuntu2004-11-7-local_11.7.1-515.65.01-1_amd64.deb
 sudo cp /var/cuda-repo-ubuntu2004-11-7-local/cuda-*-keyring.gpg /usr/share/keyrings/
 sudo apt-get update
-sudo apt-get -y install cuda
+sudo apt-get -y install cuda || true
 elif cat /etc/*release | grep ^NAME | grep Red; then
 echo "-----------------------------------------------"
 echo "Installing packages $yum_url on CentOS"
